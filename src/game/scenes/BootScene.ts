2252cfc725c6466d21a37b0a883c6445889247e8
inkgift2025 <inkgift2025@gmail.com>
2026-06-19 15:44:28 +0800

    Revert "Support direct play launch"
    This reverts commit 0be07f25f0eeb3bb84c14b23d8a929a4496cc219.

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -55,15 +55,7 @@ export class BootScene extends Phaser.Scene {
     }
 
     this.createAnimations();
-    this.scene.start(this.shouldAutoStartRun() ? 'RunScene' : 'StartScene');
-  }
-
-  private shouldAutoStartRun(): boolean {
-    if (typeof window === 'undefined') {
-      return false;
-    }
-
-    return new URLSearchParams(window.location.search).get('play') === '1';
+    this.scene.start('StartScene');
   }
 
   private drawLoadingScreen(): void {
